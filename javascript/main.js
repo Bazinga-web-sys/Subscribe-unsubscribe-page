b4887b499011f2a88d2a45e1599a2d60a958e743
Bazinga-web-sys <luka.hacek2004@gmail.com>
2025-09-24 13:53:03 +0200

    Update_ final main.js

--- javascript/main.js
+++ javascript/main.js
@@ -11,7 +11,7 @@ LOCATION:${location}
 END:VEVENT
 END:VCALENDAR`;
 
-  const blob = new Blob([icsContent], { type: "text/calendar;charset=utf-8" }); //representa .ics file
+  const blob = new Blob([icsContent], { type: "text/calendar;charset=utf-8" });
   const link = document.createElement("a"); //dynamic creation za <a>
   link.href = URL.createObjectURL(blob); //vraća privremenu URL adresu koja pokazuje na Blob, koristi se kao href za dowload
   link.download = "appointment.ics";
@@ -40,7 +40,6 @@ document.addEventListener("DOMContentLoaded", () => {
       window.location.href = "cancelled.html";
     });
   }
-
   //confirmed page, dodavanje u kalendar
   if (calendarBtns.length > 0) {
     calendarBtns.forEach((btn) => {
@@ -50,8 +49,8 @@ document.addEventListener("DOMContentLoaded", () => {
           generateICS({
             title: "Medical Appointment",
             description: "Your appointment at Affidea Čavka",
-            start: "20240816T120000Z", // 16.08.2024 14:00 po HR vremenu = 12:00 UTC
-            end: "20240816T123000Z", // trajanje 30 min
+            start: "20240816T120000Z",
+            end: "20240816T123000Z", //diff of 30minuta
             location: "Jordanovac 99, 10000 Zagreb",
           });
         } else if (cal === "google") {
@@ -60,16 +59,19 @@ document.addEventListener("DOMContentLoaded", () => {
             "_blank"
           );
         } else if (cal === "outlook") {
-          window.open(
-            "https://outlook.live.com/calendar/0/deeplink/compose?subject=Medical+Appointment&body=Your+appointment+at+Affidea+Čavka&startdt=2024-08-16T12:00:00Z&enddt=2024-08-16T12:30:00Z&location=Jordanovac+99,+10000+Zagreb",
-            "_blank"
-          );
+          // FIX za Outlook: preuzimanje ICS file umjesto deeplink
+          generateICS({
+            title: "Affieda Čavka",
+            description: "Affieda Čavka event",
+            start: "20240816T120000Z",
+            end: "20240816T123000Z",
+            location: "Jordanovac 99, 10000 Zagreb",
+          });
         }
-
         // on-cllick check mark sawp
         const icon = btn.querySelector(".cal-icon");
         if (icon) {
-          icon.src = "assets/check.svg"; // fajl s crnom kvačicom
+          icon.src = "assets/check.svg";
           icon.alt = "Added";
         }
       });
